71a5663517787b003f23cd41a8b8c98b958abf3b
bondarevVV <90005849+bondarevVV@users.noreply.github.com>
2022-09-16 20:45:14 +0300

    1

---
+++ pizza/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizza 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Peperoni peperoni = new Peperoni();
+            peperoni.Name = "Пеперони";
+            peperoni.weight = 500;
+            peperoni.Testo = "Тонкое";
+            peperoni.price = 999;
+        }
+    }
+}
---
+++ pizza/pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizza
+{
+    internal class pizza
+    {
+        public string Name { get; set; }
+        public double weight { get; set; }
+        public string Testo { get; set; }
+        public int price { get; set; }
+    }
+}
